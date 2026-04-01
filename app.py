577f1eb93513e71c76d7b996095cba896ae4657b
Adithi Shetty <adithishetty901@gmail.com>
2026-04-01 11:14:28 +0530

    hi

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def home():
     message = ""
     if request.method == 'POST':
         name = request.form.get('name')
-        message = f"Hello, {name} 👋"
+        message = f"Hi, {name} 👋"
     return render_template('index.html', message=message)
 
 if __name__ == '__main__':
